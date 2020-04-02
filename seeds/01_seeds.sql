516d38e5a568ce7da131ab70d813b05aa799f375
BilliMorgan <shimko20@gmail.com>
2020-04-02 02:37:40 +0000

    add webApp

--- seeds/01_seeds.sql
+++ seeds/01_seeds.sql
@@ -6,6 +6,11 @@ insert into properties VALUES (1, 1, 'dvarets vakh', 'description', 'http:// ima
 insert into properties VALUES (2, 1, 'dvarets2', 'description', 'http:// image.jpeg', 'http://more_images.jpeg', 500.00,  1, 2, 3,'Georgia', 'Kahi_Kaladze', 'Batumi', 'Adjaria', '02232', 'false');
 insert into properties VALUES (3, 3, 'room_for_two', 'description', 'http:// image.jpeg', 'http://more_images.jpeg', 68.99, 0, 1, 1 ,'Canada', 'downtown', 'Toronto', 'Ontario', 'R1R1T5', 'true');
 
-insert into reservations VALUES(1, '2020-03-01', '2020-03-25', 3, 2);
+INSERT INTO reservations (guest_id, property_id, start_date, end_date) 
+VALUES (1, 1, '2018-09-11', '2018-09-26'),
+(2, 2, '2019-01-04', '2019-02-01'),
+(3, 3, '2021-10-01', '2021-10-14');
 
 insert into property_reviews VALUES (1, 2, 3, 1, 5, 'description');
+insert into property_reviews VALUES (2, 1, 3, 1, 5, 'description');
+insert into property_reviews VALUES (3, 3, 3, 1, 5, 'description');
